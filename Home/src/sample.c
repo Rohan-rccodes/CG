3cd0b52f3558974766605833da880dd817b49e32
rccdoes <itschauhan101@gmail.com>
2024-04-20 01:20:58 +0530

    updated

--- Home/src/sample.c
+++ Home/src/sample.c
@@ -1,1 +1,16 @@
+#include <graphics.h>
 #include<conio.h>
+
+int main()
+{
+    int gd = DETECT, gm;
+    char data[] = "c:\MinGW\bin";
+
+    initgraph(&gd, &gm, data);
+
+    line(20, 20, 100, 100);
+
+    getch();
+    closegraph();
+    return (0);
+}
